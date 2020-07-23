378b99a54b55291b692667818e606fe265758d52
Antonette Caldwell <pullmana8@gmail.com>
2020-07-23 14:05:53 -0500

    Removed the contents, and added rbx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-interface AppProps {}
+import 'rbx/index.css';
+import { Content } from 'rbx';
 
-function App({}: AppProps) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {
+    return (
+        <Content>
+            Hello world!
+        </Content>
+    )
 }
 
 export default App;
